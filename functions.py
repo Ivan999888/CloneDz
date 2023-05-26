576694854751492dc66f384df6a398ae78794a8c
“Ivan” <ivan.alexandrov1998@mail.ru>
2023-05-26 14:10:56 +0300

    Added func update, delete, search

--- functions.py
+++ functions.py
@@ -24,7 +24,46 @@ def find_data() -> None:
 def search(book: str, info: str) -> str:
     """Находит в списке записи по определенному критерию поиска"""
     book = book.split('\n')
+    matches = []
     for contact in book:
         if info in contact:
-            return contact
-    return 'Совпадений не найдено'
+            matches.append(contact)
+
+    num_matches = len(matches)
+    if num_matches == 0:
+        return 'Совпадений не найдено'
+    elif num_matches == 1:
+        return matches[0]
+    else:
+        print('Найдено несколько совпадений:')
+        for i, match in enumerate(matches, start=1):
+            print(f'{i}. {match}')
+
+        while True:
+            choice = input('Введите номер желаемого совпадения: ')
+            if choice.isdigit() and 1 <= int(choice) <= num_matches:
+                return matches[int(choice) - 1]
+            else:
+                print('Неверный выбор. Пожалуйста, введите корректный номер.')
+
+def update_contact(book: str, name: str, new_contact: str) -> str:
+    """Обновляет контакт в телефонном справочнике по имени или фамилии"""
+    lines = book.split('\n')
+    updated_lines = []
+    for line in lines:
+        if name.lower() in line.lower():
+            updated_lines.append(new_contact)
+        else:
+            updated_lines.append(line)
+    updated_book = '\n'.join(updated_lines)
+    return updated_book
+
+def delete_contact(book: str, name: str) -> str:
+    """Удаляет контакт из телефонного справочника по имени или фамилии"""
+    lines = book.split('\n')
+    updated_lines = []
+    for line in lines:
+        if name.lower() not in line.lower():
+            updated_lines.append(line)
+    updated_book = '\n'.join(updated_lines)
+    return updated_book
